cdeb6bf459f45ae09dcd051293a8e4089663786c
Frédéric Péters <fpeters@entrouvert.com>
2017-06-13 08:56:03 +0200

    remove migration-ts1 scripts

--- src/scripts/migration-ts1/copy_categories.sh
+++
@@ -1,7 +0,0 @@
-# SAMPLE : sh copy_categories.sh lalouviere lescommunes.be
-if ! [ -d /var/lib/wcs-au-quotidien/$1-formulaires.$2/categories ]
-then
-    (mkdir /var/lib/wcs-au-quotidien/$1-formulaires.$2/categories)
-fi
-
-cp categories/* /var/lib/wcs-au-quotidien/$1-formulaires.$2/categories
--- src/scripts/migration-ts1/copy_datasources.sh
+++
@@ -1,8 +0,0 @@
-# SAMPLE : sh copy_categories.sh lalouviere lescommunes.be
-if ! [ -d /var/lib/wcs-au-quotidien/$1-formulaires.$2/datasources ]
-then
-    (mkdir /var/lib/wcs-au-quotidien/$1-formulaires.$2/datasources)
-fi
-
-sed "s~http://local-passerelle.example.net~https://$1-passerelle.$2~g" datasources/98 > /var/lib/wcs-au-quotidien/$1-formulaires.$2/datasources/98
-sed "s~http://local-passerelle.example.net~https://$1-passerelle.$2~g" datasources/99 > /var/lib/wcs-au-quotidien/$1-formulaires.$2/datasources/99
