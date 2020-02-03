f5d54ba0702b3b5bafacc90691384ee8a65136e4
Benoit Suttor <bsuttor@imio.be>
2020-02-03 10:00:31 +0100

    Add update_oidc.sh script

---
+++ sso/update_oidc.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -x
+
+wget -O agents.json "http://infra-api.imio.be/sso/plone/agents/$ENVIRONMENT" && chmod 777 agents.json
+wget -O usagers.json "http://infra-api.imio.be/sso/plone/usagers/$ENVIRONMENT" && chmod 777 usagers.json
+
+authentic2-multitenant-manage tenant_command wc-base-import -d "$AGENTS_HOSTNAME" agents.json --no-dry-run NO_DRY_RUN
+authentic2-multitenant-manage tenant_command wc-base-import -d "$USAGERS_HOSTNAME" usagers.json --no-dry-run NO_DRY_RUN
+
