09307f99142616849ba16e6cfffb193bee189844
Benoit Suttor <bsuttor@imio.be>
2020-07-08 10:54:34 +0200

    Use multi tenants (agents and usagers)

--- config/authentic2/config.py
+++ config/authentic2/config.py
@@ -16,53 +16,52 @@
 DEBUG = True
 TEMPLATE_DEBUG = True
 
-ADMINS = (
-  ('Admins IMIO', 'admints@example.net'),
-)
+ADMINS = (("Admins IMIO", "adminwc@imio.be"),)
 
 # ALLOWED_HOSTS must be correct in production!
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
-    'local-auth.example.net',
+    "agents.wc.localhost",
+    "usagers.wc.localhost",
 ]
 
 # Databases
 # Default: a local database named "authentic"
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 # Warning: don't change ENGINE
-DATABASES['default']['NAME'] = 'authentic2'
-DATABASES['default']['USER'] = 'postgres'
-DATABASES['default']['PASSWORD'] = 'password'
-DATABASES['default']['HOST'] = 'database'
-DATABASES['default']['PORT'] = '5432'
+DATABASES["default"]["NAME"] = "authentic2"
+DATABASES["default"]["USER"] = "postgres"
+DATABASES["default"]["PASSWORD"] = "password"
+DATABASES["default"]["HOST"] = "database"
+DATABASES["default"]["PORT"] = "5432"
 
 CACHES = {
-    'default': {
-        'BACKEND': 'hobo.multitenant.cache.TenantCache',
-        'REAL_BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '172.17.0.1:11211',
+    "default": {
+        "BACKEND": "hobo.multitenant.cache.TenantCache",
+        "REAL_BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
+        "LOCATION": "172.17.0.1:11211",
     }
 }
 
 
-LANGUAGE_CODE = 'fr-be'
-TIME_ZONE = 'Europe/Brussels'
+LANGUAGE_CODE = "fr-be"
+TIME_ZONE = "Europe/Brussels"
 
 # Sentry / Raven configuration
-#RAVEN_CONFIG = {
+# RAVEN_CONFIG = {
 #    'dsn': '',
-#}
+# }
 
 # Email configuration
-EMAIL_SUBJECT_PREFIX = '[authentic local_authentic2]'
-SERVER_EMAIL = 'authentic2@example.net'
-DEFAULT_FROM_EMAIL = 'authentic2@example.net'
+EMAIL_SUBJECT_PREFIX = "[authentic local_authentic2]"
+SERVER_EMAIL = "authentic2@example.net"
+DEFAULT_FROM_EMAIL = "authentic2@example.net"
 
 # SMTP configuration
-EMAIL_HOST = 'localhost'
-#EMAIL_HOST_USER = ''
-#EMAIL_HOST_PASSWORD = ''
-#EMAIL_PORT = 25
+EMAIL_HOST = "localhost"
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_PORT = 25
 
 # HTTPS Security
 CSRF_COOKIE_SECURE = False
@@ -70,21 +69,21 @@ SESSION_COOKIE_SECURE = False
 
 # Idp
 # SAML 2.0 IDP
-#A2_IDP_SAML2_ENABLE = False
+# A2_IDP_SAML2_ENABLE = False
 # CAS 1.0 / 2.0 IDP
-#A2_IDP_CAS_ENABLE = False
+# A2_IDP_CAS_ENABLE = False
 # OpenID 1.0 / 2.0 IDP
-#A2_IDP_OPENID_ENABLE = False
-MELLON_ADAPTER = ('authentic2_auth_fedict.adapters.AuthenticAdapter',)
+# A2_IDP_OPENID_ENABLE = False
+MELLON_ADAPTER = ("authentic2_auth_fedict.adapters.AuthenticAdapter",)
 
 # Authentifications
-#A2_AUTH_PASSWORD_ENABLE = True
-#A2_SSLAUTH_ENABLE = False
+# A2_AUTH_PASSWORD_ENABLE = True
+# A2_SSLAUTH_ENABLE = False
 
 HOBO_ROLE_EXPORT = True
-BROKER_URL = 'amqp://guest:guest@rabbitmq:5672/'
+BROKER_URL = "amqp://guest:guest@rabbitmq:5672/"
 
-LOCALE_PATHS = ('/var/lib/authentic2/locale',) + LOCALE_PATHS
+LOCALE_PATHS = ("/var/lib/authentic2/locale",) + LOCALE_PATHS
 
 from django import forms
 import re
@@ -99,7 +98,7 @@ class RrnField(forms.CharField):
             if (97 - int(value[:9]) % 97) != int(value[-2:]):
                 raise ValueError()
         except ValueError:
-            raise forms.ValidationError('Format invalide')
+            raise forms.ValidationError("Format invalide")
 
 
 class NumHouseField(forms.CharField):
@@ -111,7 +110,7 @@ class NumHouseField(forms.CharField):
             if not re.match("[1-9][0-9]*", value):
                 raise ValueError()
         except ValueError:
-            raise forms.ValidationError('Format invalide')
+            raise forms.ValidationError("Format invalide")
 
 
 class NumPhoneField(forms.CharField):
@@ -123,23 +122,11 @@ class NumPhoneField(forms.CharField):
             if not re.match("^(0|\\+|00)(\d{8,})", value):
                 raise ValueError()
         except ValueError:
-            raise forms.ValidationError('Format invalide')
+            raise forms.ValidationError("Format invalide")
 
 
 A2_ATTRIBUTE_KINDS = [
-        {
-            'label': u'Numéro de registre national',
-            'name': 'rrn',
-            'field_class': RrnField,
-        },
-        {
-            'label': u'Numéro de maison',
-            'name': 'num_house',
-            'field_class': NumHouseField,
-        },
-        {
-            'label': u'Numéro de téléphone',
-            'name': 'phone',
-            'field_class': NumPhoneField,
-        }
+    {"label": u"Numéro de registre national", "name": "rrn", "field_class": RrnField,},
+    {"label": u"Numéro de maison", "name": "num_house", "field_class": NumHouseField,},
+    {"label": u"Numéro de téléphone", "name": "phone", "field_class": NumPhoneField,},
 ]
