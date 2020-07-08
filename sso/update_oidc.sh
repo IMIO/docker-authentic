d8faaf6c034c6c666a449b11c19ebbd529ff6068
Benoit Suttor <bsuttor@imio.be>
2020-07-08 10:56:34 +0200

    Add oidc for testing

--- sso/update_oidc.sh
+++ sso/update_oidc.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -x
 
-wget -O agents.json "http://infra-api.imio.be/sso/agents/$ENVIRONMENT" && chmod 777 agents.json
-wget -O usagers.json "http://infra-api.imio.be/sso/usagers/$ENVIRONMENT" && chmod 777 usagers.json
+wget -O agents.json "https://infra-api.imio.be/sso/agents/$ENVIRONMENT" && chmod 777 agents.json
+wget -O usagers.json "https://infra-api.imio.be/sso/usagers/$ENVIRONMENT" && chmod 777 usagers.json
 
 authentic2-multitenant-manage tenant_command wc-base-import -d "$AGENTS_HOSTNAME" agents.json --no-dry-run NO_DRY_RUN
 authentic2-multitenant-manage tenant_command wc-base-import -d "$USAGERS_HOSTNAME" usagers.json --no-dry-run NO_DRY_RUN
-
