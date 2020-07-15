bec49842757b53ca5c6e08b7ced4ab1f67ee821a
Benoit Suttor <bsuttor@imio.be>
2020-07-15 16:56:40 +0200

    First step to start easily testing env

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
-pull:
-		docker-compose pull
+#!/usr/bin/make -f
 
 run:
-		docker-composel up
+	docker-compose up -d authentic
 
-up: run
+up: 
+	docker-compose up
 
 build:
 		docker-compose build --pull authentic
@@ -23,7 +23,20 @@ add-user:
 create-plone4-site:
 	docker-compose run --rm -v docker-authentic_plone4-data:/data plone4 buildout install plonesite
 
-add-oidc:
-	 docker-compose exec authentic bash -c 'authentic2-multitenant-manage tenant_command wc-base-import -d agents.wc.localhost agents.json --no-dry-run NO_DRY_RUN'
-	 docker-compose exec authentic bash -c 'authentic2-multitenant-manage tenant_command wc-base-import -d usagers.wc.localhost usagers.json --no-dry-run NO_DRY_RUN'
+wait-until-started:
+	until [ -d data/combo/backoffice-usagers.wc.localhost ]; do echo "waiting..."; sleep 5; done
+	echo "Tenants created" 
+	sleep 5
 
+add-oidc:
+	docker-compose exec authentic bash -c 'authentic2-multitenant-manage tenant_command wc-base-import -d agents.wc.localhost agents.json --no-dry-run NO_DRY_RUN'
+	docker-compose exec authentic bash -c 'authentic2-multitenant-manage tenant_command wc-base-import -d usagers.wc.localhost usagers.json --no-dry-run NO_DRY_RUN'
+
+set-agents-admin-to-default-ou:
+	docker-compose exec authentic bash -c 'authentic2-multitenant-manage tenant_command runscript /opt/publik/scripts/create-user.py -d agents.wc.localhost'
+
+add-usagers-user:
+	docker-compose exec authentic bash -c 'authentic2-multitenant-manage tenant_command runscript /opt/publik/scripts/create-usagers-user.py -d usagers.wc.localhost'
+
+testing-env: run wait-until-started set-agents-admin-to-default-ou add-usagers-user add-oidc
+	@echo "testing"
--- src/scripts/create-user.py
+++ src/scripts/create-user.py
@@ -1,66 +1,29 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ObjectDoesNotExist
 from django_rbac.utils import get_role_model, get_ou_model
 from hobo.agent.authentic2.provisionning import provisionning
 
-import argparse
-import hashlib
-
 
 def create_authentic_user():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-u', '--username', dest='username',
-    #                    type=str, default='bsuttor',
-    #                    help='User name'
-    #                    )
-    # parser.add_argument('-e', '--email', dest='email',
-    #                    type=str, default='bsu@imio.be',
-    #                    help='User e-mail'
-    #                    )
-    # parser.add_argument('-p', '--password', dest='password',
-    #                    type=str, default='password',
-    #                    help='User password'
-    #                    )
-    # args = parser.parse_args()
-
-    # username = args.username
-    # email = args.email
-    # password = args.password
-    username = "admin"
-    email = "bsu+wcl@imio.be"
-    password = "admin"
-
     User = get_user_model()
     OU = get_ou_model()
     Role = get_role_model()
     organisation_unit = OU.objects.get(default=True)
     with provisionning:
-        try:
-            role_admin = Role.objects.get(
-                name="Administrateur de tous les utilisateurs", ou=organisation_unit
-            )
-        except ObjectDoesNotExist:
-            role_admin = Role(
-                name="Administrateur de tous les utilisateurs", ou=organisation_unit
-            )
-        role_admin.save()
+        role_admin, created = Role.objects.get_or_create(
+            name="Administrateur de tous les utilisateurs", ou=organisation_unit
+        )
 
         with open("/tmp/tmp_uuid_agent_admin.txt", "w") as f:
             f.write(role_admin.uuid)
 
-        # Create default user with default organisation unit.
-        user_admin = User(username=username, email=email, ou=organisation_unit)
-        user_admin.set_password(create_password(password))
+        # Set default user with default organisation unit.
+        user_admin = User.objects.get(username="admin")
+        user_admin.ou = organisation_unit
         user_admin.save()
 
         # Set role to user
         role_admin.members.add(user_admin)
-
-
-def create_password(password):
-    m = hashlib.md5(password.encode("utf8"))
-    return m.hexdigest()
 
 
 create_authentic_user()
