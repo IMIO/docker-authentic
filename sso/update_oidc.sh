94199ef212e313de63403d11e0ee610dbf040abb
Benoit Suttor <bsuttor@imio.be>
2021-09-28 16:55:29 +0200

    Use new api to get services

--- sso/update_oidc.sh
+++ sso/update_oidc.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 set -x
 
-wget -O agents.json "https://infra-api.imio.be/sso/agents/$ENVIRONMENT" && chmod 777 agents.json
-wget -O usagers.json "https://infra-api.imio.be/sso/usagers/$ENVIRONMENT" && chmod 777 usagers.json
+if [ "$ENVIRONMENT" == "staging" ]
+then
+    echo "staging"
+    wget -O agents.json "https://api-staging.imio.be/imio/authentic/v1/services?tenant=agents&env=$ENVIRONMENT"  && chmod 777 agents.json
+    wget -O usagers.json "https://api-staging.imio.be/imio/authentic/v1/services?tenant=usagers&env=$ENVIRONMENT"  && chmod 777 usagers.json
+fi
+if [ "$ENVIRONMENT" == "production" ]
+then
+    echo "production"
+    wget -O agents.json "https://api.imio.be/imio/authentic/v1/services?tenant=agents&env=$ENVIRONMENT"  && chmod 777 agents.json
+    wget -O usagers.json "https://api.imio.be/imio/authentic/v1/services?tenant=usagers&env=$ENVIRONMENT"  && chmod 777 usagers.json
+fi
 
 authentic2-multitenant-manage tenant_command wc-base-import -d "$AGENTS_HOSTNAME" agents.json --no-dry-run NO_DRY_RUN
 authentic2-multitenant-manage tenant_command wc-base-import -d "$USAGERS_HOSTNAME" usagers.json --no-dry-run NO_DRY_RUN
