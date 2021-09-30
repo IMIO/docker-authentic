3536b959ff353bab73037dd3279bf6259ba3e414
Benoit Suttor <bsuttor@imio.be>
2021-09-30 10:31:06 +0200

    feat(oidc): update script to update oidc clients, this script is called by cron

--- sso/update_oidc.sh
+++ sso/update_oidc.sh
@@ -4,14 +4,14 @@ set -x
 if [ "$ENVIRONMENT" == "staging" ]
 then
     echo "staging"
-    wget -O agents.json "https://api-staging.imio.be/imio/authentic/v1/services?tenant=agents&env=$ENVIRONMENT"  && chmod 777 agents.json
-    wget -O usagers.json "https://api-staging.imio.be/imio/authentic/v1/services?tenant=usagers&env=$ENVIRONMENT"  && chmod 777 usagers.json
+    wget -O agents.json "https://api-staging.imio.be/imio/authentic/v1/services?tenant=agents"  && chmod 777 agents.json
+    wget -O usagers.json "https://api-staging.imio.be/imio/authentic/v1/services?tenant=usagers"  && chmod 777 usagers.json
 fi
 if [ "$ENVIRONMENT" == "production" ]
 then
     echo "production"
-    wget -O agents.json "https://api.imio.be/imio/authentic/v1/services?tenant=agents&env=$ENVIRONMENT"  && chmod 777 agents.json
-    wget -O usagers.json "https://api.imio.be/imio/authentic/v1/services?tenant=usagers&env=$ENVIRONMENT"  && chmod 777 usagers.json
+    wget -O agents.json "https://api.imio.be/imio/authentic/v1/services?tenant=agents"  && chmod 777 agents.json
+    wget -O usagers.json "https://api.imio.be/imio/authentic/v1/services?tenant=usagers"  && chmod 777 usagers.json
 fi
 
 authentic2-multitenant-manage tenant_command wc-base-import -d "$AGENTS_HOSTNAME" agents.json --no-dry-run NO_DRY_RUN
