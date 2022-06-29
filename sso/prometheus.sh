cee1dd10eaeeea2e3d9193b513d1a346ced212d7
Benoit Suttor <benoit.suttor@imio.be>
2022-06-29 16:41:28 +0200

    feat(monitor): add monitor service

--- sso/prometheus.sh
+++ sso/prometheus.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
-
-/usr/bin/prometheus-uwsgi-exporter.py
-mv /var/lib/prometheus/node-exporter/uwsgi.prom  /var/lib/prometheus/node-exporter/uwsgi-$INSTANCE_NAME.prom
-
-/usr/bin/prometheus-system-exporter.py > /var/lib/prometheus/node-exporter/system-$INSTANCE_NAME.prom
+while true;
+do
+    sleep 5m
+    authentic2-multitenant-manage shell -d "$AGENTS_HOSTNAME" < /usr/bin/prometheus_user_exporter.py > /var/lib/prometheus/node-exporter/authentic_users.prom
+done;
